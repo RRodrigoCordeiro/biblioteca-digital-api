0acc8ec6883cbe81b0d68a4180491539cc7c79dd
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2025-09-15 09:38:48 -0300

    ✨ feat: criar módulo de usuários conectado ao banco de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from 'src/books/books.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule,UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
